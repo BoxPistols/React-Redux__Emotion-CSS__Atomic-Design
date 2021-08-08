e8ba38c24e3942937c483024c2ffe24301659b2e
A_ito <BoxPistols@users.noreply.github.com>
2021-08-08 17:01:49 +0900

    Add Click Toggle UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import styled from "@emotion/styled";
 import { css } from "@emotion/css";
 import * as ui from "./AtomicDesign/Utility/ui";
 
 export const App = () => {
+  const [isSelected, setIsSelected] = useState(false);
+
   return (
-    <DivEmWrap>
+    <DivEmWrap bc>
       <h1 className={em_css__color}>React Atomic Emotion</h1>
+      {/* useState Toggle Style */}
+      <button
+        className={styles({ isSelected })}
+        onClick={() => setIsSelected(!isSelected)}
+      >
+        Click here to change styles.
+      </button>
     </DivEmWrap>
   );
 };
@@ -19,6 +29,23 @@ const em_css__color = css`
 
 /* Emotion Styled */
 const DivEmWrap = styled.div`
-    ${ui.tx_center}
+    ${ui.tx_center}    
     /* ${ui.fx_center} */
-  `;
+    `;
+
+/* Toggle Style useState */
+const styles = ({ isSelected }) => css`
+  border: solid 2px gray;
+  padding: 8px 16px;
+  background: none;
+  border-radius: 24px;
+  color: white;
+  cursor: pointer;
+  transition: 0.2s;
+  ${isSelected === true &&
+  `
+    border-color: hotpink;
+    background-color: #413F42;
+    color: hotpink;
+  `}
+`;
