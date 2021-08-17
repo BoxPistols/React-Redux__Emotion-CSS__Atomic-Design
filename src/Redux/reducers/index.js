4fcfdf1eb9a780b06ea5fd54b778d05efa678c8c
A_ito <BoxPistols@users.noreply.github.com>
2021-08-18 04:01:50 +0900

    redux action add title

--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { count } from "./counter";
+import { title } from "./title";
 
 export default combineReducers({
-  count
+  count,
+  title
 });
---
+++ src/Redux/reducers/title.js
@@ -0,0 +1,6 @@
+export const title = (state = "The Counter", action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
